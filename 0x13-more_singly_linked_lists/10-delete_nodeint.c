dbf82d4e1e47e53cc7c93f9a6c843a4d7b4d3d01
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-02 07:24:13 -0700

    modified some codes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 	node_to_delete = *head
-	*head = (*head)->next;
+	'*head' = (*head)->next;
 	free(node_to_delete);
 	return (1);
 	}
