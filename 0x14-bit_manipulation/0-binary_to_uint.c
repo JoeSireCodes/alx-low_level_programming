350e922530381411f76db5875b12fe8c24f9c38f
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-04 06:55:36 -0700

    task 0 updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
-	size_t len = strlen(b);
+
+	int i;
 	unsigned int result = 0;
 
-	for (size_t i = 0; i < len; i++)
+	for (i = 0; b[i]; i++)
 	{
-	if (b[i] == '0' || b[i] == '1')
+		if (b[i] < '0' || b[i] > '1')
 	{
 		result = (result << 1) + (b[i] - '0');
 	}
