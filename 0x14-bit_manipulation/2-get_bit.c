523100f7d3afe7771f10e738935adf97a47b1813
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-04 08:45:42 -0700

    modified task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= 63)
 		return (-1);
 
-	value_of_bit = 1UL >> index;
+	value_of_bit = (n >> index) & 1;
 
 	return (value_of_bit);
 }
