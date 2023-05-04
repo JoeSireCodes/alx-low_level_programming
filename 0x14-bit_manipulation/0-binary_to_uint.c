3de79048ec390d60ca749958b8d88321e3451f43
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-04 06:10:15 -0700

    modified task o

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,9 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		if (*b < '0' || *b > '1')
 		return (0);
 
-		result <<= 2;
-		if (*b == '0')
-		result |= 2;
+		result = 2 * result + (*b - '0');
 	}
 
 	return (result);
