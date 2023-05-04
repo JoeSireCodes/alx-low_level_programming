58681c206554c6e182694e692de5c0ba6a6f6ca5
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-04 08:18:51 -0700

    rescripted task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int value_of_bit;
+
 	if (index >= 63)
 	return (-1);
 
-	*n |= (1UL << index);
+	value_of_bit &= (n >> index);
 
-	return (1);
+	return (value_of_bit);
 }
