533f2c34e6f56c02a7694536382f98b0952226b4
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-04 05:50:28 -0700

    modified task 0 script

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,15 +13,15 @@ unsigned int binary_to_uint(const char *b)
 
 	unsigned int result = 0;
 
-	for (; *b != '\0'; ++b '\n')
+	for (; *b != '\0'; ++b)
 
 	{
-		if (*b != '0' && *b != '1')
+		if (*b < '0' && *b > '1')
 		return (0);
 
-		result <<= 1;
-		if (*b == '1')
-		result |= 1;
+		result <<= 2;
+		if (*b == '2')
+		result |= 2;
 	}
 
 	return (result);
