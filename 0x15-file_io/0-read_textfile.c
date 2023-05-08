a82986e5c920681116d5ab3baa5e3ed53e732c92
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-08 09:17:27 -0700

    fixed task 0 code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,21 +15,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	FILE *fp = fopen(filename, "r");
 
 	if (fp == NULL)
 	{
 		return (0);
 	}
+	char *buffer = (char *) malloc(letters + 1);
 
-	char *buffer = (char*) malloc(letters + 1);
-
-	if (buffer == NULL) {
+	if (buffer == NULL)
+	{
 		fclose(fp);
 		return (0);
 	}
-
 	ssize_t bytes_read = fread(buffer, sizeof(char), letters, fp);
 
 	if (bytes_read <= 0 || bytes_read != letters)
@@ -38,8 +36,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		fclose(fp);
 		return (0);
 	}
-
 	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
 	free(buffer);
 	fclose(fp);
 
