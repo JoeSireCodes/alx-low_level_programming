20bb027b4b0af19662a0e89caa33f22c27d610c6
JoeSireCodes <jsamogulu@yahoo.com>
2023-05-09 06:31:56 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
 
 /**
  * create_file - Creates a file.
@@ -15,31 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd, q, len = 0;
+
 	if (filename == NULL)
 		return (-1);
-
-	int flags = O_CREAT | O_RDWR | O_TRUNC;
-
-	int fd = open(filename, flags);
-
-	if (fd == -1 || w == -1)
-		return (-1);
-
-	size_t len = 0;
 
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	q = write(fd, text_content, len);
 
-	ssize_t bytes_written = write(fd, text_content, len);
-
-	if (bytes_written == -1 || bytes_written != (ssize_t)len)
-	{
-		close(fd);
+	if (fd == -1 || q == -1)
 		return (-1);
-	}
 
 	close(fd);
 	return (1);
